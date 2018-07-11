d6217e9593be94d6c7898344cc6e3b14fa8b7887
Katherine Boedges <kboedges@me.com>
2018-07-11 14:45:39 -0500

    Adding New Deck content

--- components/DeckQuiz.js
+++ components/DeckQuiz.js
@@ -4,11 +4,12 @@ import { midGray, lightestGray } from "../utils/colors";
 
 export default class DeckQuiz extends React.Component {
   render() {
+    const { navigation } = this.props;
+    const deck = navigation.getParam("deck", "NO DECK");
+
     return (
       <View style={styles.container}>
-        <View>
-          <Text>Maybe create a new stack here??</Text>
-        </View>
+        <View>{deck.questions.map(question => <Text key={question.question}>{question.question}</Text>)}</View>
       </View>
     );
   }
--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView } from "react-native";
+import { midGray } from "../utils/colors";
 
 export default class NewDeck extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>NewDeck</Text>
-      </View>
+      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+        <Text style={styles.title}>What is the title of your new deck?</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={text => this.setState({ text })}
+          value={this.state.text}
+          placeholder="Deck title"
+          placeholderTextColor={midGray}
+        />
+        <TouchableOpacity style={styles.button} onPress={this.onPress}>
+          <Text style={[styles.buttonText, { color: "white" }]}>Submit</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -16,6 +32,36 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "space-around"
+  },
+  textInput: {
+    height: 40,
+    fontSize: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: midGray,
+    alignSelf: "stretch",
+    marginLeft: 10,
+    marginRight: 10,
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  title: {
+    fontSize: 45,
+    paddingBottom: 10,
+    color: "black",
+    textAlign: "center"
+  },
+  button: {
+    borderRadius: 5,
+    backgroundColor: "black",
+    paddingRight: 30,
+    paddingLeft: 30,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginBottom: 20
+  },
+  buttonText: {
+    fontSize: 30
   }
 });
