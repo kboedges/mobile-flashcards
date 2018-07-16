06f2afcd29f8ea7d3b49ba1e4a6aac0780554fcb
Katherine Boedges <kboedges@me.com>
2018-07-15 22:20:55 -0500

    Setting up bones for addQuestion

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView } from "react-native";
+import { StyleSheet, Text, TextInput, TouchableOpacity, KeyboardAvoidingView } from "react-native";
 import { midGray } from "../utils/colors";
 import { connect } from "react-redux";
 
--- components/NewQuestion.js
+++ components/NewQuestion.js
@@ -1,12 +1,42 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { midGray } from "../utils/colors";
+import { StyleSheet, Text, TextInput, TouchableOpacity, KeyboardAvoidingView } from "react-native";
 
 export default class NewQuestion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: "",
+      answer: ""
+    };
+  }
+
+  // onPress = newDeckTitle => {
+  //   this.props.addDeck(newDeckTitle).then(this.props.navigation.navigate("DeckList"));
+  //   this.setState({ text: "" });
+  // };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>New Question</Text>
-      </View>
+      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={question => this.setState({ question })}
+          value={this.state.question}
+          placeholder="Card question"
+          placeholderTextColor={midGray}
+        />
+        <TextInput
+          style={styles.textInput}
+          onChangeText={answer => this.setState({ answer })}
+          value={this.state.answer}
+          placeholder="Card answer"
+          placeholderTextColor={midGray}
+        />
+        <TouchableOpacity style={styles.button} onPress={() => this.onPress(this.state.text)}>
+          <Text style={[styles.buttonText, { color: "white" }]}>Submit</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -16,6 +46,36 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "space-around",
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  textInput: {
+    height: 40,
+    fontSize: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: midGray,
+    alignSelf: "stretch",
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  title: {
+    fontSize: 40,
+    paddingBottom: 10,
+    color: "black",
+    textAlign: "center"
+  },
+  button: {
+    borderRadius: 5,
+    backgroundColor: "black",
+    paddingRight: 30,
+    paddingLeft: 30,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginBottom: 20
+  },
+  buttonText: {
+    fontSize: 30
   }
 });
